d316523f92596dcaee51a242e6b07ededc185b68
elisaflemer <elisaflemer@gmail.com>
2023-05-19 17:05:35 -0300

    comentado

--- turtleController.py
+++ turtleController.py
@@ -12,40 +12,31 @@ MAX_DIFF = 0.1
 
 
 class Pose(TPose):
-    """
-    Essa classe serve só para que eu possa modificar alguns
-    dos comportamentos default da classe Pose do turtlesim.
-    """
+    # Classe para guardar poses do robô no mundo
 
     def __init__(self, x=0.0, y=0.0, theta=0.0):
         super().__init__(x=x, y=y, theta=theta)
         
     def __repr__(self):
-        """Modifica a forma como aparece a classe no `print`"""
+
         return f"(x={self.x:.2f}, theta={self.y:.2f})"
     
     def __add__(self, other):
-        """Overload do operador `+`"""
         self.x += other.x
         self.y += other.y
         return self
     
     def __sub__(self, other):
-        """Overload do operador `-`"""
         self.x -= other.x
         self.y -= other.y
         return self
     
     def __eq__(self, other):
-        """
-        Modifica a funcionalidade do operador `==`
-        Passa a considerar como iguais duas poses que 
-        estiverem dentro de um range delimitado por `MAX_DIFF`.
-        A comparação considera x, y ao mesmo tempo.
-        """
+        # Checa igualdade dentro da margem de segurança
         return abs(self.x - other.x) < MAX_DIFF \
         and abs(self.y - other.y) < MAX_DIFF \
 
+# Classe de pilha (FILO)
 class Stack():
     def __init__(self):
         self.stack = []
@@ -56,6 +47,7 @@ class Stack():
     def pop(self):
         return self.stack.pop()
 
+# Classe de controle da rota, em fila
 class MissionControl(deque):
     def __init__(self):
         super().__init__()
@@ -73,19 +65,12 @@ class MissionControl(deque):
         return super().popleft()
 
 class TurtleController(Node):
-    """
-    Classe do controlador. É um nó de ROS
-    """
+    # Controlador da tartaruga
     
     def __init__(self, mission_control, control_period=0.02, stack = Stack()):
-        """
-        Construtor da classe controladora.
-        Aqui cria-se o publisher, a subscription e o timer.
-        Como parâmetro é possível passar o período do controlador.
-        """
+        # Recebe a rota em fila, o período de controle e uma fila para guardar a rota já percorrida
         super().__init__('turtle_controller')
-        # Aproveitei a classe Pose para meus parâmetros pose e setpoint
-        # Iniciei ambos em x=-40.0 pois é um valor impossível no turtlesim
+
         self.pose = Pose(x=-40.0)
         self.setpoint = Pose(x=-40.0)
         self.mission_control = mission_control
@@ -102,29 +87,28 @@ class TurtleController(Node):
             callback=self.pose_callback,
             qos_profile=10
         )
-        # O timer passa a ser um timer do loop de controle.
-        # Não se deve iniciar loop de controle sem definir um período.
+
+
         self.control_timer = self.create_timer(
                 timer_period_sec=control_period,
                 callback=self.control_callback
         )
 
     def control_callback(self):
-        """
-        Loop de controle. 
-        Aqui é onde define-se o acionamento dos motores do robô.
-        Roda a cada `control_period` segundos.
-        """
+        # Controla movimentação do robô
+
         # Se a pose estiver em -40.0, significa que não chegou nenhuma info ainda
         if self.pose.x == -40.0:
             self.get_logger().info("Aguardando primeira pose...")
             return
+        
+        # Compara distância do robô com o destino e envia velocidades de acordo
         msg = Twist()
         x_diff = self.setpoint.x - self.pose.x
         y_diff = self.setpoint.y - self.pose.y
         if self.pose == self.setpoint:
             msg.linear.x, msg.linear.y = 0.0, 0.0
-            self.get_logger().info(f"Mbappé chegou em {self.pose}")
+            self.get_logger().info(f"Robô chegou em {self.pose}")
             self.update_setpoint()
         if abs(y_diff) > MAX_DIFF:
             msg.linear.y = 0.4 if y_diff > 0 else -0.4
@@ -137,36 +121,36 @@ class TurtleController(Node):
         self.publisher.publish(msg)
         
     def pose_callback(self, msg):
-        """
-        Método de callback da subscrição à pose da tartaruga.
-        Toda vez que chega uma mensagem no tópico de pose,
-        esse método é executado.
-        """
+        #Método de callback da posição da tartaruga
         self.pose = Pose(x=msg.x, y=msg.y, theta=msg.theta)
-        # Se for a primeira vez passando por aqui, cria o setpoint.
+
+        # Guarda pose inicial no stack e pega primeiro setpoint
         if self.setpoint.x == -40.0:
             print(f"Pose inicial: {self.pose}")
-            self.stack.push(self.pose)
-            print("PRIMEIRO PONTO DA STACK " + str(self.stack.stack))
             self.update_setpoint()
 
+    # Atualiza o setpoint
     def update_setpoint(self):
+        # Se ainda houver elementos na fila de rota, vai para o próximo elemento
         if(len(self.mission_control) >= 1):
-            self.stack.push(self.pose)
-            x, y = self.mission_control.dequeue()
+            self.stack.push(self.pose) # Salva posição atual no stack
+            # Pega o próximo item da fila
+            x, y = self.mission_control.dequeue() 
             self.setpoint = self.pose + Pose(x, y)
             print(f'Setpoint: {self.setpoint}')
             
+        # Se a fila de pontos tiver terminado, utiliza o stack para voltar ao ponto inicial    
         elif(len(self.stack.stack) >= 1):
-            print("STACK:" + str(self.stack.stack))
             last_pose = self.stack.pop()
             self.setpoint = last_pose
             print(f'Setpoint: {self.setpoint}')
+        
+        # Se o stack tiver terminado, chegou ao destino final
         else:
             print(f"Pose final: {self.pose}")
             exit()
 
-
+# Código principal
 def main(args=None):
     rclpy.init(args=args)
     mc = MissionControl()
